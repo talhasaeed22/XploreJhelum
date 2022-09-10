2df8ad098bbfba0b9e6bed86abe62b834b38894b
talhasaeed22 <talhasaeed100@gmail.com>
2022-09-10 20:06:02 +0500

    login pages

---
+++ src/Components/Auth/SIgnup.js
@@ -0,0 +1,78 @@
+import React from 'react'
+import { useLocation, useNavigate } from 'react-router-dom'
+import logo from '../../Images/logo.png'
+import './Auth.css'
+import bg from '../../Images/blurbg.jpg'
+import lowerbg from '../../Images/bg.jpg'
+
+const SIgnup = () => {
+    let location = useLocation();
+    let navigate = useNavigate();
+    const signup = () => {
+        let path = `/Signup`;
+        navigate(path);
+    }
+    const login = () => {
+        let path = `/Login`;
+        navigate(path);
+    }
+    return (
+        <div className=" LoginContainer d-flex" style={{ backgroundImage: `url(${bg})` }}>
+            <div className='LeftLoginContainer gap-5' style={{ backgroundImage: `url(${lowerbg})` }}>
+                <div className="Lupper ">
+                    <div className="logo">
+                        <img src={logo} alt="logo" className='img-fluid' />
+                    </div>
+
+                    <div className='d-flex flex-column'>
+                        <span className='fs-1 fw-bold'>Welcome</span>
+                        <span style={{ fontSize: '18px', fontWeight: '600' }}>We recommend you to login to our app for better experience</span>
+                    </div>
+
+                </div>
+                <div className="Llower d-flex flex-column fw-bold">
+                    <div className='d-flex align-items-center gap-2'>
+                        <i className="fa fa-check-circle-o" aria-hidden="true"></i>
+                        <span>Explore whole Jhelum places hotels and many more</span>
+                    </div>
+                    <div className='d-flex align-items-center gap-2'>
+                        <i className="fa fa-check-circle-o" aria-hidden="true"></i>
+                        <span>All your needs at one place</span>
+                    </div>
+                    <div className='d-flex align-items-center gap-2'>
+                        <i className="fa fa-check-circle-o" aria-hidden="true"></i>
+                        <span>We provide secure platform</span>
+                    </div>
+                </div>
+            </div>
+            <div className="RightLoginContainer">
+                <div className='my-4'>
+                    <span className='loginMainHead'> <span style={{ cursor: 'pointer', color: location.pathname === '/Login' ? '#e41f1f' : 'black' }} onClick={login}>Login</span> / <span style={{ cursor: 'pointer', color: location.pathname === '/Signup' ? '#e41f1f' : 'black' }} onClick={signup}>Signup</span></span>
+                </div>
+
+                <div className='loginForm d-flex flex-column gap-3 '>
+                    <div className='d-flex flex-column'>
+                        <label htmlFor="name">Name</label>
+                        <input className='loginInp' type="text" name="name" id="name" placeholder='Enter your name' />
+                    </div>
+                    <div className='d-flex flex-column'>
+                        <label htmlFor="email">Email Address</label>
+                        <input className='loginInp' type="email" name="email" id="email" placeholder='abc@example.com' />
+                    </div>
+                    <div className='d-flex flex-column'>
+                        <label htmlFor="password">Password</label>
+                        <input className='loginInp' type="password" name="password" id="password" placeholder='******' />
+                    </div>
+                    <div className='mx-auto my-4'>
+                        <button className='loginBtn'>Signup</button>
+                    </div>
+                    <div>
+                        <span className='fw-bold'>Already have an account? <span style={{textDecoration:'underline', color:'#e41f1f', cursor:'pointer'}} onClick={login}>Login</span></span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default SIgnup
